e7f4179391afc0d6f8b22e9c011c007cdbf71a56
Elly Maimon and Ryan Murry <elly@>
2018-09-11 11:06:20 -0700

    Add state to Post List Header to handle new post form and to Post to handle up and downvoting

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Icon } from 'semantic-ui-react'
 
-const Post = (props) => {
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: 0
+    }
+    this.handleVoteUpClick = this.handleVoteUpClick.bind(this);
+    this.handleVoteDownClick = this.handleVoteDownClick.bind(this);
+  }
 
-  const {title, name, post, time} = props;
+  handleVoteUpClick() {
+    let currentVotes = this.state.votes;
+    currentVotes ++;
+    this.setState({votes: currentVotes});
+  }
 
-  return (
-    <div>
-      <h3>{title}</h3>
-      <p><em>By {name} at {time}</em></p>
-      <p>{post}</p>
-      <br/>
-    </div>
-  )
+  handleVoteDownClick() {
+    let currentVotes = this.state.votes;
+    currentVotes --;
+    this.setState({votes: currentVotes});
+  }
+
+  render() {
+    const {title, name, post, time} = this.props;
+    return (
+      <div>
+        <h3>{title}</h3>
+        <Icon size='large' name='hand point up outline' onClick={this.handleVoteUpClick}/>
+        <p>{this.state.votes}</p>
+        <Icon size='large' name='hand point down outline' onClick={this.handleVoteDownClick}/>
+        <p><em>By {name} at {time}</em></p>
+        <p>{post}</p>
+        <br/>
+      </div>
+    )
+  }
 }
 
-export default Post
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  post: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired
+}
+
+export default Post;
--- src/components/PostListHeader.js
+++ src/components/PostListHeader.js
@@ -3,14 +3,34 @@ import PropTypes from 'prop-types'
 import NewPostForm from './NewPostForm'
 import { Button } from 'semantic-ui-react'
 
-const PostListHeader = (props) => {
-  return (
-    <div>
-      <h1>Trade Rumors</h1>
-      <Button>New Post</Button>
-      <NewPostForm />
-    </div>
-  )
+class PostListHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    }
+    this.handleNewPostClick = this.handleNewPostClick.bind(this);
+  }
+
+  handleNewPostClick() {
+    this.setState({ formVisibleOnPage: true })
+  }
+
+  render () {
+    let currentView = null;
+
+    if(this.state.formVisibleOnPage) {
+      currentView = <NewPostForm />;
+    } else {
+      currentView = <Button onClick={ this.handleNewPostClick }>New Post</Button>;
+    }
+    return(
+      <div>
+        <h1>Trade Rumors</h1>
+        { currentView }
+      </div>
+    )
+  }
 }
 
-export default PostListHeader
+export default PostListHeader;
